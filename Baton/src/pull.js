422ad40613813a2e0eb11b495888f8fee63620f2
JohnCarter-Art <belart.ba@gmail.com>
2022-03-19 19:05:36 +0500

    2

--- Baton/src/pull.js
+++ Baton/src/pull.js
@@ -1,3 +1,4 @@
 var scores = [60, 50, 60, 58,54, 54, 58, 50, 52, 54];
 var solution2 = scores[2];
 alert("Solution 2 produced " + solution2 + " bubbles.");
+var flavors = ["vanilla", "butterscotch", "lavender", "chocolate", "cookie dough"];
