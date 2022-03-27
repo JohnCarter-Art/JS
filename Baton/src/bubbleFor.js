e0743b4587145618b268d7572184d3bd85e7fda2
unknown <belart.ba@gmail.com>
2022-03-27 19:47:51 +0500

    Added Bundler

--- Baton/src/bubbleFor.js
+++ Baton/src/bubbleFor.js
@@ -4,16 +4,26 @@ var scores = [60, 50, 60, 58, 54, 54,
     69, 64, 66, 55, 52, 61,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
-var output;
-highScore = 0;
+function printAndGetHighScore(scores) {
+    var highScore = 0;
+    var output;
+    for (var i = 0; i < scores.length; i++) {
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
+    }
+    return highScore;
+}
+var highScore = printAndGetHighScore(scores);
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+var bestSolutions = [];
 for (var i = 0; i < scores.length; i++) {
-    output = "Bubble solution #" + i + " score: " + scores[i];
-    console.log(output);
-    if (scores[i] > highScore) {
-        highScore = scores[i];
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
     }
 }
-console.log("Bubbles tests: " + i);
-console.log("Highest bubble score: " + highScore);
-var scoreMax = [];
-scoreMax
+console.log("Solutions with the highest score: " + bestSolutions);
+function getBestResults()
